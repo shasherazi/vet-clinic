8dda876984c83089080c6af6a1372cf97950f8d2
aubinleyoung <aubinsimpeze01@gmail.com>
2023-04-04 17:33:40 +0200

    CREATE invoices table

--- schema-based-on-diagram.sql
+++ schema-based-on-diagram.sql
@@ -12,3 +12,14 @@ CREATE TABLE medical_histories (
     CONSTRAINT fk_medical_histories_patients
         FOREIGN KEY (patient_id) REFERENCES patients (id)
 );
+
+CREATE TABLE invoices (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id  INT NOT NULL,
+     CONSTRAINT fk_invoices_medical_histories
+        FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id)
+
+)
