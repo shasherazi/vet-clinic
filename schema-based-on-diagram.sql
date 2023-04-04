1fad020b00dcb930ac6539fa675d06a40b28a720
shasherazi <hassanrandomz@gmail.com>
2023-04-04 20:08:06 +0500

    Create medical_histories table

--- schema-based-on-diagram.sql
+++ schema-based-on-diagram.sql
@@ -3,3 +3,12 @@ CREATE TABLE patients (
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL
 );
+
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    admitted_at TIMESTAMP NOT NULL,
+    patient_id INT NOT NULL,
+    status VARCHAR(100) NOT NULL
+    CONSTRAINT fk_medical_histories_patients
+        FOREIGN KEY (patient_id) REFERENCES patients (id)
+);
